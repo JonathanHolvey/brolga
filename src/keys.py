785fe49ecbdc97b9b1d95db21973b65575f01454
Jonathan Holvey <jonathan.holvey@outlook.com>
2019-06-15 07:27:46 +1000

    Add last used property to keystore

--- src/keys.py
+++ src/keys.py
@@ -1,6 +1,7 @@
 import json
 import string
 import random
+from datetime import datetime
 
 from passlib.hash import sha256_crypt as crypt
 
@@ -20,14 +21,23 @@ class Keystore:
         """Load the keys from the keys file"""
         try:
             with open(self.file, 'r') as f:
-                self.keys = json.load(f)
+                self.keys = self.parse(json.load(f))
         except FileNotFoundError:
             pass
+
+    def parse(self, keys):
+        """Parse timestamps from loaded keys"""
+        for key_id, key in keys.items():
+            try:
+                keys[key_id]['used'] = datetime.fromisoformat(key.get('used'))
+            except TypeError:
+                pass
+        return keys
 
     def save(self):
         """Save the keys to the keys file"""
         with open(self.file, 'w+') as f:
-            json.dump(self.keys, f, indent=2)
+            json.dump(self.keys, f, indent=2, default=str)
 
     def add(self, name):
         """Generate a new key"""
@@ -38,13 +48,15 @@ class Keystore:
         while key_id is None or key_id in self.keys.keys():
             key_id = self.random(self.ID_LENGTH).lower()
 
-        self.keys[key_id] = {'hash': key_hash, 'name': name}
+        self.keys[key_id] = {'hash': key_hash, 'name': name, 'used': False}
+        self.save()
         return '{}-{}'.format(key_id, key_value)
 
     def delete(self, key_id):
         """Delete a key by its ID"""
         try:
             del self.keys[key_id]
+            self.save()
             return True
         except KeyError:
             return False
@@ -53,7 +65,13 @@ class Keystore:
         """Verify a key against its stored hash"""
         key_id, key_value = key.split('-')
         key_hash = self.keys.get(key_id, {}).get('hash')
+        self.timestamp(key_id)
         return crypt.verify(key_value, key_hash)
+
+    def timestamp(self, key_id):
+        """Set the last used time of a key"""
+        self.keys[key_id]['used'] = datetime.now()
+        self.save()
 
     def random(self, length):
         """Generate a random string of letters and numbers"""
